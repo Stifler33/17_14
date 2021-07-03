b215954f15e1a8cd162f825131727b76c7e49bb2
stifler33 <bagi33.ru@gmail.com>
2021-07-03 14:43:17 +0300

    main3 fixed1

--- main3.cpp
+++ main3.cpp
@@ -2,19 +2,17 @@
 bool substr(char* a, char* b){
     bool finish = false;
     for (int i = 0; a[i] != '\0'; i++){
-        if (a[i] == b[0]){
-            for (int countB = 0, countA = i; b[countB] != '\0'; countB++, countA++){
+            for (int countB = 0, countA = i; b[countB] != '\0' && a[i] == b[0]; countB++, countA++){
                 if (b[countB] != a[countA]){
                     finish = false;
                     break;
                 }else finish = true;
             }
-        }
     }
     return finish;
 }
 int main(){
-    char* a = "nut Suer mup";
+    char* a = "nuSuper mup";
     char* b = "up";
     char* c = "Sup";
     char* d = "nut";
