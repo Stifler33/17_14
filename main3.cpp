2819f350d3c1138f875b01a9775094af880c0a4f
stifler33 <bagi33.ru@gmail.com>
2021-07-04 23:23:39 +0300

    main3 fixed2 finish

--- main3.cpp
+++ main3.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
 bool substr(char* a, char* b){
-    bool finish = false;
+    bool fullMatch = false;
     for (int i = 0; a[i] != '\0'; i++){
             for (int countB = 0, countA = i; b[countB] != '\0' && a[i] == b[0]; countB++, countA++){
                 if (b[countB] != a[countA]){
-                    finish = false;
                     break;
-                }else finish = true;
+                }else if (b[countB+1] == '\0') fullMatch = true;
             }
     }
-    return finish;
+    return fullMatch;
 }
 int main(){
-    char* a = "nuSuper mup";
-    char* b = "up";
-    char* c = "Sup";
-    char* d = "nut";
+    char* a = "Barshka Bumbashku";
+    char* b = "shku";
     if (substr(a,b)){
-        std::cout << "1 yes" << "\n";
-    }
-    if (substr(a,c)){
-        std::cout << "2 yes" << "\n";
-    }
-    if (substr(a,d)){
-        std::cout << "3 yes" << "\n";
-    }
+        std::cout << "yes";
+    }else std::cout << "no";
     return 0;
 }
 
